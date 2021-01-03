407cf2c9054d183283688c9201cec179c6d80552
Roni Yaniv <roniyaniv@gmail.com>
2021-01-03 09:55:06 +0200

    Basic add title and cut script (needs pre-existing comp)

---
+++ basic-add-title-and-cut.jsx
@@ -0,0 +1,62 @@
+// TODO: change this script to a command line script
+
+// For this to work, you need to have the app active and a comp selected
+var mainComp = app.project.activeItem;
+var mainLayer = mainComp.layer(1);
+
+var dlg = new Window("dialog", "Add Title and Cut", undefined, { resizeable: true });
+
+// We are creating two UI element groups: one for input, one for main buttons
+
+// Group ONE of UI elements
+var groupOne = dlg.add("group", undefined, "groupOne");
+groupOne.orientation = "column";
+
+groupOne.add("statictext", undefined, "This script allows you to add a title and cut your video.");
+var titleName = groupOne.add("edittext", undefined, "Title of Video");
+var titleDuration = groupOne.add("edittext", undefined, "Duration of Title");
+
+var cutOne = groupOne.add("edittext", undefined, "Cut Point 1");
+var cutTwo = groupOne.add("edittext", undefined, "Cut Point 2");
+
+// Group TWO of UI elements
+var groupTwo = dlg.add("group", undefined, "Buttons");
+groupTwo.orientation = "row";
+
+var startButton = groupTwo.add("button", undefined, "Start");
+var cancelButton = groupTwo.add("button", undefined, "Cancel");
+
+startButton.onClick = function () {
+    app.beginUndoGroup("Tutorial");
+    doStuff();
+}
+
+cancelButton.onClick = function () {
+    dlg.close();
+}
+
+function doStuff() {
+    var compDuration = mainComp.duration;
+    var titleSeconds = parseInt(titleDuration.text);
+    var cutOneSeconds = parseInt(cutOne.text);
+    var cutTwoSeconds = parseInt(cutTwo.text);
+
+    var titleText = mainComp.layers.addText(titleName.text);
+    titleText.outPoint = titleSeconds;
+
+    var middleLayer = mainLayer.duplicate();
+    var topLayer = middleLayer.duplicate();
+
+    mainLayer.outPoint = cutOneSeconds;
+    middleLayer.inPoint = cutOneSeconds;
+    middleLayer.outPoint = cutTwoSeconds;
+    topLayer.inPoint = cutTwoSeconds;
+    topLayer.outPoint = compDuration;
+
+    dlg.close();
+    app.endUndoGroup();
+
+}
+
+dlg.show();
+dlg.center();
